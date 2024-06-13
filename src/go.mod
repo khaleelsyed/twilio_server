39cff02e46f13a6d27122267e60a42b896ecb6a7
khaleelsyed <khaleelsyed@users.noreply.github.com>
2024-06-13 00:46:38 +0000

    feat: add token endpoint

--- src/go.mod
+++ src/go.mod
@@ -2,8 +2,6 @@ module github.com/khaleelsyed/twilio_server
 
 go 1.20
 
-require (
-	github.com/golang/mock v1.6.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/twilio/twilio-go v1.21.1 // indirect
-)
+require github.com/twilio/twilio-go v1.21.1
+
+require github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
--- src/handlers.go
+++ src/handlers.go
@@ -1,7 +1,51 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/twilio/twilio-go/client/jwt"
+)
 
 func voice(w http.ResponseWriter, r *http.Request) {}
 
-func token(w http.ResponseWriter, r *http.Request) {}
+func token(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var twilioAccountSid string = os.Getenv("TWILIO_ACCOUNT_SID")
+	var twilioApiKey string = os.Getenv("TWILIO_API_KEY")
+	var twilioApiSecret string = os.Getenv("TWILIO_API_SECRET")
+
+	var outgoingApplicationSid string = os.Getenv("TWIML_APP_SID")
+
+	params := jwt.AccessTokenParams{
+		AccountSid:    twilioAccountSid,
+		SigningKeySid: twilioApiKey,
+		Secret:        twilioApiSecret,
+		Identity:      "system",
+		Region:        "us1",
+	}
+
+	jwtToken := jwt.CreateAccessToken(params)
+	voiceGrant := &jwt.VoiceGrant{
+		Outgoing: jwt.Outgoing{
+			ApplicationSid: outgoingApplicationSid,
+		},
+		Incoming: jwt.Incoming{
+			Allow: true,
+		},
+	}
+
+	jwtToken.AddGrant(voiceGrant)
+	token, err := jwtToken.ToJwt()
+
+	if err != nil {
+		error := fmt.Errorf("error: %q", err)
+		fmt.Println(error.Error())
+	}
+
+	w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(token)
+	return
+}
